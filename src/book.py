308cbe7cbe6d481ba22d48a62354e76fd30d3b42
tau-lavender <thesigmabase@gmail.com>
2025-12-10 20:02:42 +0300

    add some Book method, add IndexDict

--- src/book.py
+++ src/book.py
@@ -1,43 +1,78 @@
-from typing import Iterator
+from typing import (
+    Iterator,
+    List
+)
 
 
 class Book():
-    def __init__(self, title: str, author: str | None, year: int | None, genre: str | None, isbn: int) -> None:
+    def __init__(self,
+                isbn: int,
+                title: str,
+                author: str | None = None,
+                year: int | None = None,
+                genre: str | None = None
+                ) -> None:
+        self.isbn = isbn
         self.title = title
         self.author = author
         self.year = year
         self.genre = genre
-        self.isbn = isbn
+
+
+    def _get_temp_str(self):
+        return str(self.isbn) + str(self.title) + str(self.author) + str(self.year) + str(self.genre)
+
+
+    def hash(self):
+        return hash(self._get_temp_str())
+
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Book):
+            return False
+        return self._get_temp_str() == value._get_temp_str()
+
 
     def __str__(self) -> str:
         return f'"{self.title}" {self.author}'
 
 
+    def __repr__(self) -> str:
+        return f'Book: ({self.isbn}) "{self.title}" {self.author} {self.year} {self.genre}'
+
+
 class BookCollection():
     def __init__(self) -> None:
-        self.books: list[Book] = []
+        self.books: List[Book] = []
 
-    def __getitem__(self, index: int | slice) -> Book | list[Book]:
+
+    def __contains__(self, book: Book) -> bool:
+        return book in self.books
+
+
+    def __getitem__(self, index: int | slice) -> Book | List[Book]:
         if type(index) not in (int, slice):
             raise IndexError(f"Wrong index ({index}) type {type(index)}")
         return self.books[index]
 
+
     def __iter__(self) -> Iterator[Book]:
         return iter(self.books)
+
 
     def __len__(self) -> int:
         return len(self.books)
 
+
     def append(self, book: Book) -> None:
         self.books.append(book)
 
-    def extend(self, books: list[Book]) -> None:
+
+    def extend(self, books: List[Book]) -> None:
         self.books.extend(books)
 
-    def is_book_exist(self, book: Book) -> bool:
-        return book in self.books
 
     def remove(self, book: Book) -> None:
-        if not self.is_book_exist(book):
+        if book not in self:
             raise ValueError(f"{book}")
         self.books.remove(book)
